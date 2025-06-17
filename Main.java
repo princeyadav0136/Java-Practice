baa7bf2a37d82c4b43844a468d726ee24cad99b7
Prince Yadav <princeyadav0136@gmail.com>
2025-06-17 14:13:35 +0530

    Encapsulation Example

--- Main.java
+++ Main.java
@@ -1,20 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Student s = new Student("Prince", 101);
-        s.displayInfo();
+        BankAccount account = new BankAccount();
+        account.setAccountNumber(123456);
+        account.setBalance(1000);
+        System.out.println("Account Number: " + account.getAccountNumber());
+        System.out.println("Balance: " + account.getBalance());
     }
 }
 
-class Student {
-    String name;
-    int roll;
-
-    Student(String name, int roll) {
-        this.name = name;
-        this.roll = roll;
-    }
-
-    void displayInfo() {
-        System.out.println("Name: " + name + ", Roll No: " + roll);
-    }
-}
