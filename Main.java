98531a32f81368f6424d5e5d347541140e0d413d
Prince Yadav <princeyadav0136@gmail.com>
2025-07-04 17:57:32 +0530

    Concrete Class

--- Main.java
+++ Main.java
@@ -1,10 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+
+        //POJO Example
         Book book1 = new Book();
         book1.setTitle("1984");
         book1.setAuthor("George Orwell");
         System.out.println("Title: " + book1.getTitle());
         System.out.println("Author: " + book1.getAuthor());
+
+        // Concrete Class Example
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(5, 3);
+        System.out.println("Sum: " + sum);
+        int difference = calculator.subtract(10, 4);
+        System.out.println("Difference: " + difference);
+        int product = calculator.multiply(6, 7);
+        System.out.println("Product: " + product);
+        double quotient = calculator.divide(20, 4);
+        System.out.println("Quotient: " + quotient);
 
     }
 }
