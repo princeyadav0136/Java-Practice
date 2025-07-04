12a860cd1672ea444f6c0594197874d0bd054f49
Prince Yadav <princeyadav0136@gmail.com>
2025-07-04 06:30:32 +0530

    Comment error

---
+++ BankCustomer.java
@@ -0,0 +1,8 @@
+public class BankCustomer extends Bank {
+    
+    // @Override
+    // public void calculateInterest() {
+        // System.out.println("Calculating interest for bank customer...");    
+    // This method is attempting to override a final method from the Bank class
+    // }
+}
